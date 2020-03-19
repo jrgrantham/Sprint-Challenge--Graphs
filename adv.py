dc71fcba9b186f922d34f7bfce3abcb86f590e45
James Grantham <james.r.grantham@gmail.com>
2020-03-19 18:00:54 +0000

    random room selection

--- adv.py
+++ adv.py
@@ -11,8 +11,8 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+# map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
@@ -31,13 +31,13 @@ player = Player(world.starting_room)
 traversal_path = []
 graph = {}
 
-def set_exits(current):
-    if not graph.get(current):
-        graph[current] = {}
-    else:
+def set_exits(room):
+    if graph.get(room):
         return
+    else:
+        graph[room] = {}
     for exit in player.current_room.get_exits():
-        graph[current][exit] = '?'
+        graph[room][exit] = '?'
 
 def connect_rooms(previous, current, direction):
     set_exits(current)
@@ -45,8 +45,18 @@ def connect_rooms(previous, current, direction):
     graph[previous][direction] = current 
     graph[current][pairs[direction]] = previous
 
+def unexplored_exits(room):
+    results = []
+    for exit in player.current_room.get_exits():
+        if graph[room][exit] == '?':
+            results.append(exit)
+    return results
+
 set_exits(player.current_room.id)
 previous_room = player.current_room.id
+
+room_queue = Queue()
+room_queue.enqueue(player.current_room)
 
 print('-' * 20)
 print(f'you are in room {player.current_room.id}')
@@ -54,15 +64,18 @@ print(f'your options are {player.current_room.get_exits()}')
 print('-' * 20)
 
 while len(graph) < len(world.rooms):
+    move = random.choice(unexplored_exits(player.current_room.id))
+    player.travel(move)
+    traversal_path.append(move)
+    connect_rooms(previous_room, player.current_room.id, move)
     print('-' * 20)
-    player.travel('n')
     print(f'you were in room {previous_room}')
     print(f'you are now in room {player.current_room.id}')
     print(f'your options are {player.current_room.get_exits()}')
-    traversal_path.append('n')
-    connect_rooms(previous_room, player.current_room.id, 'n')
-    previous_room = player.current_room.id
+    print(f'rooms visited {graph[player.current_room.id]}')
+    print(f'unvisited {unexplored_exits(player.current_room.id)}')
     print('-' * 20)
+    previous_room = player.current_room.id
 
 print(graph)
 
